2e4a25b228cd56d4319913893266a69373c21e87
midsane <adityaraj10544@gmail.com>
2025-02-19 22:25:41 +0000

    fixing minor bugs

---
+++ client/src/components/screenAreaTxt.tsx
@@ -0,0 +1,33 @@
+import { SplitText } from "./textAnimations/splitText"
+
+
+export const ScreeAreaTxt: React.FC<{
+    title: string,
+    border?: boolean,
+    width?: string,
+    paddingBottom?: string,
+    borderRadius?: string
+}> = ({
+    title,
+    border = false,
+    width = "100%",
+    borderRadius = "8px",
+    paddingBottom = "0" }) => {
+        return <div
+            style={{ width, paddingBottom, borderRadius }}
+            className={`${border && "border-b"} items-center border-secondaryLight h-20 bg-primary absolute top-0 left-0 px-4 pt-5 z-[60] flex justify-between `}>
+          <div className="text-accent" >
+            <p >{">"}</p>
+            <SplitText
+                text={title}
+                className="text-2xl text-white font-semibold text-center"
+                delay={150}
+                animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
+                animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+                threshold={0.2}
+                rootMargin="-50px"
+            />
+          </div>
+            
+        </div>
+    }
