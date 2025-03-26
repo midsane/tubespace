2644fe033fa50a4f1b38f56e009647868dd30bea
midsane <adityaraj10544@gmail.com>
2025-03-26 07:28:04 +0000

    fixing server build issues, changing RequestType

--- server/src/types/types.ts
+++ server/src/types/types.ts
@@ -2,7 +2,11 @@ import { Request } from "express";
 
 export interface RequestType extends Request {
     user?: any;
-    body: any;    
-    cookies: any; 
-    header: any; 
+    body: any;
+    cookies: any;
+    header: any;
+    query: any;
+    file: any;
+    files: any;
+    params: any;
 }
